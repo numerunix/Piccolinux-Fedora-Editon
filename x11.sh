63ba2fe3d982559ae63ecb000a1cd6a8bb251c9e
Giulio Sorrentino <49764967+numerunix@users.noreply.github.com>
2020-11-29 00:43:51 +0100

    1.0-guido
    - Modificato il controllo per vedere se si è su fedora
    - Modificato il sistema in interfaccia testuale
    - Viene richiesto il riavvio

--- x11.sh
+++ x11.sh
@@ -1,55 +1,74 @@
 #!/bin/bash
 #autore Giulio Sorrentino <gsorre84@gmail.com>
 
+function prerequisti {
+	dnf install dialog
+}
+
+function splashScreen {
+	dialog --title "Informazioni" --backtitle "Informazioni" --msgbox "Giulio Sorrentino <gsorre84@gmail.com> copyright 2020\nIl programma viene concesso in licenza secondo la licenza GPL v3 o, secondo il tuo parere, qualsiasi versione successiva.\nIl programma viene concesso COME E', senza NESSUNA garanzia né esplicita né implicita.\nSe ti piace considera una donazione al mio paypal." 60 40
+}
+
+function isNotRoot  {
 if  [[ $EUID -ne 0 ]]; then
-	echo "Lo script va avviato da root."
-	exit 1;
+	dialog --title "Errore" --backtitle "Errore" --msgbox "Il programma va avviato da root." 60 40
+	return 1
+fi
+	return 0
+}
+
+function getDestkop {
+dialog --title "Seleziona il desktop" --backtitle "Seleziona il desktop" --radiolist "Selezionare quale desktop installare" 60 40 9 \
+1 "Gnome" true \
+2 "Kde" false \
+3 "Xfce" false \
+4 "LXDE" false \
+5 "LXQt" false \
+6 "Cinnamon" false \
+7 "Mate" false \
+8 "Sugar" false \
+9 "Deepin" false > /dev/tty 2>/tmp/result.txt
+if [ $? -eq 0 ]; then
+	desktop=`cat /tmp/result.txt`
+else
+	desktop=0;
+fi
+rm /tmp/result.txt
+return $desktop
+}
+
+splashScreen
+
+if [ ! -f "/etc/fedora-release" ]; then
+	dialog --title "Errore" --backtitle "Errore" --msgbox "Non sei su fedora." 60 40
+	exit 1
 fi
 
-versione=`cat /etc/fedora-release | cut -d" " -f3`
-if  [[ $versione -ne 33 ]]; then
-	echo "Non sei su fedora 33. Il programma non può continuare."
-	exit 1;
+prerequisti
+isNotRoot
+if [ $? -eq 1 ]; then
+	exit 1
 fi
 
-echo "1. Gnome
-2. KDE
-3. Xfce
-4. Lxde
-5. LxQt
-6. Cinnamon
-7. Mate
-8. Sugar
-9. Deepin
-Selezionare quale desktop installare: "
-
-read desktop;
-
-case $desktop in
-	1) desktop="Desktop Base"
-	;;
-	2) desktop="Spazi di lavoro Plasma per KDE"
-	;;
-	3) desktop="Desktop Xfce"
-	;;
-	4) desktop="Desktop LXDE"
-	;;
-	5) desktop="Desktop LXQt"
-	;;
-	6) desktop="Desktop Cinnamon"
-	;;
-	7) desktop="Desktop Mate"
-	;;
-	8) desktop="Ambiente Desktop Sugar"
-	;;
-	9) desktop="Deepin Desktop"
-	;;
-	*) echo "Scelta non valida. Il programma esce."
-		exit 1
-	;;
+getDestkop
+case $? in
+	1) desktop="Desktop Base";;
+	2) desktop="Spazi di lavoro Plasma per KDE";;
+	3) desktop="Desktop Xfce";;
+	4) desktop="Desktop LXDE";;
+	5) desktop="Desktop LXQt";;
+	6) desktop="Desktop Cinnamon";;
+	7) desktop="Desktop Mate";;
+	8) desktop="Ambiente Desktop Sugar";;
+	9) desktop="Deepin Desktop";;
+	*) dialog --title "Attenzione" --backtitle "Attenzione" --msgbox "L'operazione e' stata annullata." 60 40
+	exit 1;;
 esac
 
 dnf groupinstall "$desktop"
 systemctl set-default graphical.target
-echo "Il sistema è stato installato. Riavviare per avere il login grafico."
+dialog --title "OK" --backtitle "OK" --yesno "Il sistema e' stato installato. Vuoi riavviare?" 60 40
+if [ $? -eq 0 ]; then
+systemctl reboot
+fi
 
